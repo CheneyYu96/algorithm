0827a3ccb278a5aa4ae77397a147b3318dc7bc51
ymcfy <709832926@qq.com>
2017-04-24 23:43:13 +0800

    O(n) time and O(n) space

--- src/ReorderList.java
+++ src/ReorderList.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author yuminchen
  * @version V1.0
@@ -5,7 +8,7 @@
  */
 public class ReorderList {
     public void reorderList(ListNode head) {
-        if(head.next==null || head.next.next==null){
+        if(head==null||head.next==null || head.next.next==null){
             return;
         }
         ListNode last = head;
@@ -25,14 +28,44 @@ public class ReorderList {
         head.next = last;
         reorderList(second);
     }
+
+    public void spaceForTime(ListNode head){
+        if(head==null||head.next==null || head.next.next==null){
+            return;
+        }
+        List<ListNode> nodes = new ArrayList<>();
+        ListNode next = head;
+        while (next != null){
+            nodes.add(next);
+            next = head.next;
+            head.next = null;
+            head = next;
+        }
+        int len = nodes.size() -1 ;
+        head = nodes.get(0);
+        head.next = nodes.get(len);
+        head = head.next;
+        for(int i = 1; i <= (len-1)/2; i++){
+            head.next = nodes.get(i);
+            head = head.next;
+            head.next = nodes.get(len-i);
+            head = head.next;
+        }
+
+        if(len%2==0){
+            head.next = nodes.get(len/2);
+        }
+    }
+
     public static void main(String[] args) {
         ListNode h = new ListNode(1);
         ListNode cur = h;
-        for(int i = 2; i < 5; i++){
+        for(int i = 2; i <= 7; i++){
             cur.next = new ListNode(i);
             cur = cur.next;
         }
-        new ReorderList().reorderList(h);
+        ListNode first = h;
+        new ReorderList().spaceForTime(first);
         while (h!=null){
             System.out.print(h.val);
             h = h.next;
