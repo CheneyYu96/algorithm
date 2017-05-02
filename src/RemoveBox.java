9211e7bb353b1692b0af26447c9862a99de6c6c1
ymcfy <709832926@qq.com>
2017-05-02 11:25:02 +0800

    update, but still not all test pass. todo

--- src/RemoveBox.java
+++ src/RemoveBox.java
@@ -30,12 +30,25 @@ public class RemoveBox {
         for(int i = left; i < right; i++){
             if(boxes[i] == leftValue){
                 indexes.add(i);
-                int tmp = 0;
-                for(int j = 0; j < indexes.size()-1; j++){
-                    tmp += removeSubBoxes(mem, boxes, indexes.get(j) + 1, indexes.get(j + 1));
+//                int tmp = 0;
+//                for(int j = 0; j < indexes.size()-1; j++){
+//                    tmp += removeSubBoxes(mem, boxes, indexes.get(j) + 1, indexes.get(j + 1));
+//                }
+//                tmp += removeSubBoxes(mem, boxes, indexes.get(indexes.size()-1) + 1, right);
+//                tmp += indexes.size() * indexes.size();
+//                max = Math.max(max, tmp);
+            }
+        }
+
+        int size = indexes.size();
+        for(int i = 0; i < size; i++){
+            for(int j = i; j < size; j++){
+                int tmp = removeSubBoxes(mem, boxes, left, indexes.get(i));
+                for(int k = i; k < j; k++){
+                    tmp += removeSubBoxes(mem, boxes, indexes.get(k) + 1, indexes.get(k + 1));
                 }
-                tmp += removeSubBoxes(mem, boxes, indexes.get(indexes.size()-1) + 1, right);
-                tmp += indexes.size() * indexes.size();
+                tmp += removeSubBoxes(mem, boxes, indexes.get(j) + 1, right);
+                tmp += (j - i + 1) * (j - i + 1);
                 max = Math.max(max, tmp);
             }
         }
@@ -44,6 +57,6 @@ public class RemoveBox {
     }
 
     public static void main(String[] args) {
-        System.out.println(new RemoveBox().removeBoxes(new int[]{1, 3, 2, 2, 2, 3, 4, 3, 1}));
+        System.out.println(new RemoveBox().removeBoxes(new int[]{3, 8, 8, 5, 5, 3, 9, 2, 4, 4, 6, 5, 8, 4, 8, 6, 9, 6, 2, 8, 6, 4, 1, 9, 5, 3, 10, 5, 3, 3, 9, 8, 8, 6, 5, 3, 7, 4, 9, 6, 3, 9, 4, 3, 5, 10, 7, 6, 10, 7}));
     }
 }
