eae40bade00f392a8d59c265850105b2f038d2fb
ymcfy <709832926@qq.com>
2017-05-22 17:57:40 +0800

    reverse method for post order traveral

--- src/PostOrderTree.java
+++ src/PostOrderTree.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -36,9 +38,42 @@ public class PostOrderTree {
             }
         }
         return ret;
-
     }
+
+    public List<Integer> postOrder(TreeNode root){
+        List<Integer> ret = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.empty()){
+            TreeNode node = stack.pop();
+            ret.add(node.val);
+            if(node.left != null){
+                stack.push(node.left);
+            }
+            if(node.right != null){
+                stack.push(node.right);
+            }
+        }
+
+        Collections.reverse(ret);
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        n1.left = n2;
+        n1.right = n3;
+        n3.right = n4;
+        System.out.println(new PostOrderTree().postorderTraversal(n1));
+        System.out.println(new PostOrderTree().postOrder(n1));
+    }
+
 }
+
 
 class TreeNode {
       int val;
